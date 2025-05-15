4c3626b74afe3ee75ba059c49802abb28f0eef03
pritipriya-dasbehera <100078891+pritipriya-dasbehera@users.noreply.github.com>
2025-05-15 19:28:29 +0000

    Deploying to gh-pages from @ pritipriya-dasbehera/pritipriya-dasbehera.github.io@3a3b25226849b0d9a340ced6255b9c1f07fd3e67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,12 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-work-on-website-started",
+        },{id: "news-work-on-website-started",
           title: 'Work on website started',
           description: "",
           section: "News",},{id: "projects-interactive-izhikevich-neuron-model",
